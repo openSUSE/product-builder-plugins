0d9bf1780760e20222503652790599c3f4ea347a
Adrian Schröter <adrian@suse.de>
2021-08-18 14:16:15 +0200

    fix writemodulemd call

--- KIWIDescrPlugin.pm
+++ KIWIDescrPlugin.pm
@@ -262,6 +262,7 @@ sub createRepositoryMetadata {
     }
 
     if (-e "$masterpath/repodata/repomd.xml") {
+
       $this->addLicenseFile($masterpath, "license");
       foreach my $product (@{$coll->{m_products}}) {
         $this->logMsg("I", "Check for $product license file");
@@ -269,18 +270,18 @@ sub createRepositoryMetadata {
       }
 
       # rpm-md modulemd files
-      if (-e "_modulemd.in") {
-         $cmd = "/usr/lib/build/writemodulemd --filter _modulemd.in > $masterpath/repodata/modules.yaml";
+      if (-e "/usr/src/packages/SOURCES/_modulemd.pst") {
+         $cmd = "/usr/lib/build/writemodulemd --filter /usr/src/packages/SOURCES/_modulemd.pst $masterpath/repodata > $masterpath/repodata/modules.yaml";
          $call = $this -> callCmd($cmd);
          $status = $call->[0];
-         my $out = join("\n",@{$call->[1]});
+         my $out = join("\n",@{$call->[2]});
          $this->logMsg("I",
              "Called $cmd exit status: <$status> output: $out"
          );
          $cmd = "/usr/bin/modifyrepo $masterpath/repodata/modules.yaml $masterpath/repodata/";
          $call = $this -> callCmd($cmd);
          $status = $call->[0];
-         $out = join("\n",@{$call->[1]});
+         $out = join("\n",@{$call->[2]});
          $this->logMsg("I",
              "Called $cmd exit status: <$status> output: $out"
          );
